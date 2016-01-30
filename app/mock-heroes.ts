b66ac85cf5ba097c92a6ea582845d26c46bfa80a
hudecsamuel <hudecsamuel@gmail.com>
2016-01-30 18:57:21 +0100

    Completed angular 2 quickstart

---
+++ app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import {Hero} from './hero.ts';
+
+export var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Mergado"},
+  { "id": 12, "name": "Dude"},
+  { "id": 12, "name": "Huxley"},
+  { "id": 13, "name": "Sagan"},
+  { "id": 14, "name": "Neil OG"},
+  { "id": 15, "name": "Dr. IQ"},
+  { "id": 16, "name": "Hulk"},
+  { "id": 17, "name": "Musk"},
+  { "id": 18, "name": "Jobs"},
+  { "id": 19, "name": "Lazarus Long"},
+  { "id": 20, "name": "Mahen"},
+  { "id": 21, "name": "CoolGuy"}
+]
